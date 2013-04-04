940b55a84e76cbab485aaa142ed0f9f912e1c236
toogy <contact@toogy.fr>
2013-04-04 21:22:38 +0200

    networking

---
+++ TROFF/TROFF/TROFF/Network/Events/Event.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TROFF.Network.Events
+{
+    abstract class Event
+    {
+        public byte PacketOpCode { get; private set; }
+
+        protected Event(byte packetOpCode)
+        {
+            PacketOpCode = packetOpCode;
+        }
+    }
+}
---
+++ TROFF/TROFF/TROFF/Network/Events/SyncEvent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using TROFF.Play;
+
+namespace TROFF.Network.Events
+{
+    class SyncEvent : Event
+    {
+        public Point Position;
+        public Direction Direction;
+
+        public SyncEvent() : base(0)
+        {
+        }
+    }
+}
